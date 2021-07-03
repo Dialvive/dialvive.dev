e7f69cb21e63b12d0d9584767df1770bb9f0f2c3
dialvive <dialvive@protonmail.com>
2021-07-02 21:11:17 -0500

    Test utterances color change

--- assets/js/theme.js
+++ assets/js/theme.js
@@ -36,7 +36,7 @@ function toggleTheme() {
   setTheme(newTheme);
   localStorage.setItem(STORAGE_KEY, newTheme);
   const utterancesTheme = newTheme === themes.DARK ? "github-light" : "github-dark";
-  document.getElementsByTagName("iframe").contentWindow.postMessage({ type: "set-theme", theme: utterancesTheme },  "*")
+  document.getElementsByTagName("iframe")[0].contentWindow.postMessage({ type: "set-theme", theme: utterancesTheme },  "*")
 }
 
 function getTheme() {
